46de89fda122d92a91c339f2cbb86d7fad5e5d85
AlmirPaulo <ap.freelas@gmail.com>
2020-12-12 12:44:50 -0300

    only html and css static page

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from browser import document as doc
 from browser import html 
+from browser.template import Template
 
+#Title and Text
 title = html.H1("Front-End, Bots, Scraping and Crawlers (Ethical) development")
-text = html.P("Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque veritatis nemo magnam consectetur laborum repellat nobis recusandae, rem exercitationem inventore enim aperiam qui deserunt aut distinctio tempora itaque dicta similique?")
+#text = html.P({text}, id='text')
+
+#Change language
+br_btn = html.INPUT(id='br', type='checkbox')
+en_btn = html.INPUT(id='en', type='checkbox')
+label_br = html.LABEL(For='br')
+label_en = html.LABEL(For='en')
+br_flag = html.IMG(src='brazil.png')
+us_flag = html.IMG(src='united-states.png')
+
+label_br <= br_flag
+label_en <= us_flag
+
+
 main = html.MAIN()
 
-main <= title + text 
+main <= title + label_br + label_en + br_btn + en_btn 
 
 doc["body"] <= main
